ce39c8659a6ee382ddfdb5f3eeef7c1bbf8c0f6c
bokhuuu <zurabokhua@gmail.com>
2026-03-30 16:05:09 +0400

    feat: add AdminUserSeeder with role assignment

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminUser = User::firstOrCreate(
+            ['email' => 'zura.coding@gmail.com'],
+            [
+                'name' => 'zura',
+                'password' => Hash::make('zura123')
+            ]
+        );
+
+        $adminUser->assignRole(UserRole::ADMIN->value);
+    }
+}
