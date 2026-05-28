9502273c5c09ca495627005d3305e874f898359b
bokhuuu <zurabokhua@gmail.com>
2026-05-28 14:54:52 +0400

    chore: regenerate Wayfinder routes for Telescope

--- resources/js/actions/Laravel/index.ts
+++ resources/js/actions/Laravel/index.ts
@@ -1,9 +1,11 @@
 import Fortify from './Fortify'
 import Horizon from './Horizon'
+import Telescope from './Telescope'
 
 const Laravel = {
     Fortify: Object.assign(Fortify, Fortify),
     Horizon: Object.assign(Horizon, Horizon),
+    Telescope: Object.assign(Telescope, Telescope),
 }
 
 export default Laravel
